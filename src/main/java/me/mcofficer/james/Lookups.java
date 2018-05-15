0a64fba9f766b08f412dc0d0faf8c6e4820c9723
M*C*O <mcofficer@gmx.de>
2018-05-15 07:46:16 +0200

    ability to lookup and print text (sprites will follow)

--- src/main/java/me/mcofficer/james/Lookups.java
+++ src/main/java/me/mcofficer/james/Lookups.java
@@ -1,6 +1,7 @@
 package me.mcofficer.james;
 
 import me.mcofficer.esparser.DataFile;
+import me.mcofficer.esparser.DataNode;
 
 import java.util.ArrayList;
 
@@ -10,5 +11,39 @@ public class Lookups {
 
     public Lookups(ArrayList<DataFile> dataFiles) {
         this.dataFiles = dataFiles;
+    }
+
+    public DataNode getNodeByString(String query) {
+        query = query.toLowerCase();
+        int shortest = Integer.MAX_VALUE;
+        DataNode match = null;
+
+        // preselect, assuming that the user did not make a typo
+        for (DataFile file : dataFiles) {
+            for (DataNode node : file.getNodes()) {
+                String tokens = String.join(" ", node.getTokens()).trim().toLowerCase();
+                query = query.toLowerCase();
+
+                if(tokens.contains(query) && tokens.length() < shortest) {
+                    shortest= tokens.length();
+                    match = node;
+                }
+            }
+        }
+        return match;
+    }
+
+    public String getNodeAsText(DataNode node) {
+        return printNodeRecursive(node);
+    }
+
+    private String printNodeRecursive(DataNode node) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n")
+                .append(String.join(" ", node.getTokens()).trim());
+        for(DataNode child : node.getChildren())
+            sb.append("\t\t")
+                    .append(printNodeRecursive(child).replace("\n", "\n\t"));
+        return sb.toString();
     }
 }
