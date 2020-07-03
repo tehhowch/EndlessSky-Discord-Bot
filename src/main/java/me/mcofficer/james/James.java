2c124b843471e45b0b44b1b00ab287f991c33f80
M*C*O <mcofficer@gmx.de>
2020-07-03 18:15:05 +0200

    Update JDA to v4.2.0, use all gateway intents

--- src/main/java/me/mcofficer/james/James.java
+++ src/main/java/me/mcofficer/james/James.java
@@ -21,6 +21,7 @@ import me.mcofficer.james.tools.Translator;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import okhttp3.OkHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +31,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.Properties;
 
 public class James {
@@ -76,7 +78,7 @@ public class James {
 
         clientBuilder.setHelpConsumer(new Help(clientBuilder.build())); // this HAS to be done after adding all Commands!
 
-        JDA jda = JDABuilder.createDefault(cfg.getProperty("token"))
+        JDA jda = JDABuilder.create(cfg.getProperty("token"), EnumSet.allOf(GatewayIntent.class))
                 .addEventListeners(clientBuilder.build(), eventWaiter)
                 .build()
                 .awaitReady();
