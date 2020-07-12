574fda83c46df2255edfbdab27077d094583dd43
M*C*O <mcofficer@gmx.de>
2020-07-12 11:52:58 +0200

    Expand `-activity` to show a list of members

--- src/main/java/me/mcofficer/james/commands/moderation/Activity.java
+++ src/main/java/me/mcofficer/james/commands/moderation/Activity.java
@@ -4,15 +4,11 @@ import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import me.mcofficer.james.James;
 import me.mcofficer.james.Util;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageHistory;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.utils.MiscUtil;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.utils.TimeUtil;
 
 import java.time.Instant;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Activity extends Command {
 
@@ -20,8 +16,8 @@ public class Activity extends Command {
 
     public Activity(String[] ontopicCategories) {
         name = "activity";
-        arguments = "X";
-        help = "Provides stats about the activity of a Member X in the ontopic channels. For Mods only, has 30 seconds cooldown.";
+        arguments = "[member]";
+        help = "Provides stats about Member Activity over the last 2 weeks in ontopic channels. For Mods only, has 30 seconds cooldown.";
         cooldown = 30;
         category = James.moderation;
         this.ontopicCategories = ontopicCategories;
@@ -35,21 +31,51 @@ public class Activity extends Command {
             return;
         }
 
-        Member query = event.getMessage().getMentionedMembers().get(0);
-
         long timestamp = Instant.now().minusSeconds(1209600L).toEpochMilli(); // 2 weeks ago
         String discordTimestamp = Long.toUnsignedString(TimeUtil.getDiscordTimestamp(timestamp));
 
-        ArrayList<Message> messages = new ArrayList<>();
-        for (String c : ontopicCategories)
-            for (TextChannel channel : event.getJDA().getCategoryById(c).getTextChannels())
-                messages.addAll(MessageHistory.getHistoryBefore(channel, discordTimestamp).complete().getRetrievedHistory());
+        List<Message> messages = new ArrayList<>();
+        for (String catId : ontopicCategories) {
+            net.dv8tion.jda.api.entities.Category category = event.getJDA().getCategoryById(catId);
+            if (category != null) {
+                for (TextChannel channel : category.getTextChannels()) {
+                    messages.addAll(MessageHistory.getHistoryBefore(channel, discordTimestamp).complete().getRetrievedHistory());
+                }
+            }
+            else {
+                event.reply("Failed to find category with ID " + catId + ", results will be inaccurate. Please contact my hoster.");
+            }
+        }
 
-        int count = 0;
-        for (Message msg : messages)
-            if (msg.getMember().equals(query))
-                count++;
+        Map<Member, Integer> results = new HashMap<>();
+        for (Message msg : messages) {
+            if (msg.getMember()!=null) {
+                results.merge(msg.getMember(), 1, Integer::sum);
+            }
+        }
 
-        event.reply(query.getAsMention() + " sent " + count + " Messages in Ontopic-Channels over the last 2 weeks.");
+        try {
+            Member query = event.getMessage().getMentionedMembers().get(0);
+            if (results.containsKey(query)) {
+                event.reply(query.getEffectiveName() + " sent " + results.getOrDefault(query, 0) + " Messages in Ontopic-Channels over the last 2 weeks.");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            int maxLength = results
+                    .keySet()
+                    .stream()
+                    .max(Comparator.comparingInt(o -> o.getEffectiveName().length()))
+                    .orElseThrow()
+                    .getEffectiveName()
+                    .length();
+            String formatString = "%-" + (maxLength + 5) + "s%04d\n";
+
+            List<String> tableLines = new ArrayList<>();
+            for (Map.Entry<Member, Integer> entry : results.entrySet()) {
+                Member member = entry.getKey();
+                int sent = entry.getValue();
+                tableLines.add(String.format(formatString, member.getEffectiveName(), sent));
+            }
+            Util.sendInChunks(event.getTextChannel(), tableLines, "```", "```");
+        }
     }
 }
